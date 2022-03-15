bc47a25a07974bd9c4fa20c65dd3dc958a7875a8
Karrthik Arya <200020068@iitb.ac.in>
2022-03-15 19:49:29 +0530

    Preliminary design created

---
+++ src/components/Navbars/AdminNavbar.js
@@ -0,0 +1,88 @@
+/*!
+
+=========================================================
+* Black Dashboard React v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+import codewars_logo from "../../assets/img/codewars_logo.png"
+// reactstrap components
+import {
+  Button,
+  Collapse,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  Input,
+  InputGroup,
+  NavbarBrand,
+  Navbar,
+  NavLink,
+  Nav,
+  Container,
+  Modal,
+  NavbarToggler,
+  ModalHeader,
+} from "reactstrap";
+
+function AdminNavbar(props) {
+  const [collapseOpen, setcollapseOpen] = React.useState(false);
+  const [modalSearch, setmodalSearch] = React.useState(false);
+  const [color, setcolor] = React.useState("navbar-transparent");
+  // function that adds color white/transparent to the navbar on resize (this is for the collapse)
+  // this function opens and closes the collapse on small devices
+  const toggleCollapse = () => {
+    if (collapseOpen) {
+      setcolor("navbar-transparent");
+    } else {
+      setcolor("bg-white");
+    }
+    setcollapseOpen(!collapseOpen);
+  };
+  // this function is to open the Search modal
+  const toggleModalSearch = () => {
+    setmodalSearch(!modalSearch);
+  };
+  return (
+    <>
+      <Navbar className={classNames("navbar-absolute", color)} expand="lg">
+        <Container fluid>
+          <div className="navbar-wrapper">
+            <NavbarBrand href="#pablo" onClick={(e) => e.preventDefault()}>
+              CodeWars
+            </NavbarBrand>
+          </div>
+          
+          <NavbarToggler onClick={toggleCollapse}>
+            <span className="navbar-toggler-bar navbar-kebab" />
+            <span className="navbar-toggler-bar navbar-kebab" />
+            <span className="navbar-toggler-bar navbar-kebab" />
+          </NavbarToggler>
+          <Collapse navbar isOpen={collapseOpen}>
+            <Nav className="mx-auto" navbar>
+            <div className="logo">
+            <img src = {codewars_logo} height="40px"></img>
+          </div>
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default AdminNavbar;
